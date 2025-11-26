0be6edd9b2cb3f1566d37a391236c2174c754ef6
csuhqf <csuhqf@gmail.com>
2025-11-27 00:03:06 +0800

    add treesitter

---
+++ lua/plugins/treesitter.lua
@@ -0,0 +1,29 @@
+return {
+    {
+        "nvim-treesitter/nvim-treesitter",
+        build = ":TSUpdate", -- 安装后自动更新解析器
+        config = function()
+            local configs = require("nvim-treesitter.configs")
+
+            configs.setup({
+                -- 确保安装这些语言的解析器
+                ensure_installed = { 
+                    "c", "lua", "vim", "vimdoc", "query", --这是 Neovim 必须的
+                    "python", "markdown", "markdown_inline" --这是你需要用的
+                },
+                
+                -- 自动安装缺少的解析器
+                auto_install = true,
+
+                -- 启用代码高亮模块
+                highlight = {
+                    enable = true,
+                    additional_vim_regex_highlighting = false,
+                },
+
+                -- 启用基于 Treesitter 的缩进 (对 Python 很有用)
+                indent = { enable = true },
+            })
+        end,
+    }
+}
