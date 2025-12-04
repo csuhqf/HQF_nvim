fdf728e682e5ffa859052b0485e60e71ab10f6d2
csuhqf <csuhqf@gmail.com>
2025-12-04 14:28:48 +0800

    refactor: revert lsp config to standard stable setup

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -4,7 +4,8 @@ return {
 		dependencies = {
 			"williamboman/mason-lspconfig.nvim",
 			"neovim/nvim-lspconfig",
-			"WhoIsSethDaniel/mason-tool-installer.nvim",
+			"WhoIsSethDaniel/mason-tool-installer.nvim", -- 保留：自动安装工具
+			"hrsh7th/cmp-nvim-lsp",
 		},
 		config = function()
 			-- 1. 初始化 Mason
@@ -18,56 +19,82 @@ return {
 				},
 			})
 
-			-- 2.1 自动安装 LSP 服务器
+			-- 2. 自动安装工具 (保留)
 			require("mason-tool-installer").setup({
 				ensure_installed = {
 					"black",
 					"isort",
-					"prettier",
+					"prettier", -- 格式化工具
 				},
 			})
 
-			-- 2.2 配置 Mason-LSPConfig (关键修改点)
-			-- 我们不再手动调用 lspconfig.pyright.setup，而是使用 "handlers"
-			-- 这样 Mason 会自动用正确的方式去启动 LSP，避免新版本的报错
+			-- 3. 准备 Capabilities
+			-- 保留 snippetSupport 是为了让 CSS/HTML 的基础补全更丰富，这是无副作用的优化
+			local capabilities = require("cmp_nvim_lsp").default_capabilities()
+			capabilities.textDocument.completion.completionItem.snippetSupport = true
+
+			-- 4. 配置 LSP
 			require("mason-lspconfig").setup({
 				ensure_installed = {
 					"lua_ls",
 					"pyright",
 					"html",
 					"cssls",
-					"emmet_language_server", -- 输入 div 等标签时自动补全
+					"emmet_language_server",
 					"ts_ls",
 				},
-
-				-- 这里的 handlers 是自动处理所有 LSP 的设置
 				handlers = {
-					-- 默认处理程序：对于所有列表里的 LSP，直接启动
+					-- A. 默认处理器
 					function(server_name)
-						require("lspconfig")[server_name].setup({})
+						require("lspconfig")[server_name].setup({
+							capabilities = capabilities,
+						})
 					end,
 
-					-- 特殊处理程序：针对 lua_ls 需要特殊配置
+					-- B. Lua 配置
 					["lua_ls"] = function()
-						require("lspconfig").lua_ls.setup({})
+						require("lspconfig").lua_ls.setup({
+							capabilities = capabilities,
+							settings = {
+								Lua = { diagnostics = { globals = { "vim" } } },
+							},
+						})
+					end,
+
+					-- C. Emmet 配置 (保留常用 Web 类型)
+					["emmet_language_server"] = function()
+						require("lspconfig").emmet_language_server.setup({
+							capabilities = capabilities,
+							filetypes = {
+								"html",
+								"css",
+								"javascript",
+								"javascriptreact",
+							},
+						})
+					end,
+
+					-- D. JS/TS 配置 (回归标准)
+					-- 不再强制它去读 HTML，只处理标准的 JS/TS/React 文件
+					["ts_ls"] = function()
+						require("lspconfig").ts_ls.setup({
+							capabilities = capabilities,
+							-- 只要是 js/ts/jsx/tsx 它都会工作
+						})
 					end,
 				},
 			})
 
-			-- 3. 全局快捷键 (保持不变)
-			-- 当检测到 LSP 启动后，自动加载这些快捷键
+			-- 5. 全局快捷键
 			vim.api.nvim_create_autocmd("LspAttach", {
 				group = vim.api.nvim_create_augroup("UserLspConfig", {}),
 				callback = function(ev)
 					local opts = { buffer = ev.buf }
-					-- gd: 跳转定义
 					vim.keymap.set("n", "gd", vim.lsp.buf.definition, opts)
-					-- K: 查看文档
 					vim.keymap.set("n", "K", vim.lsp.buf.hover, opts)
-					-- <Space>rn: 重命名
 					vim.keymap.set("n", "<Leader>rn", vim.lsp.buf.rename, opts)
-					-- <Space>ca: 代码行为 (Code Action, 比如自动修复 imports)
 					vim.keymap.set("n", "<Leader>ca", vim.lsp.buf.code_action, opts)
+					vim.keymap.set("n", "gr", require("telescope.builtin").lsp_references, opts)
 				end,
 			})
 		end,
