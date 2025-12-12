862802be8770949caba0b959010799beb7bbccd7
csuhqf <csuhqf@gmail.com>
2025-12-12 10:32:15 +0800

    feat: finalize UI aesthetics (dashboard, bufferline, jelly cursor)

--- lua/plugins/bufferline.lua
+++ lua/plugins/bufferline.lua
@@ -1,52 +1,76 @@
 return {
 	{
 		"akinsho/bufferline.nvim",
-		dependencies = { "nvim-tree/nvim-web-devicons" }, -- 依赖图标
+		dependencies = {
+			"nvim-tree/nvim-web-devicons",
+			"echasnovski/mini.bufremove",
+		},
 		version = "*",
-		event = "VeryLazy", -- 延迟加载，不拖慢启动速度
+		event = "VeryLazy",
 		config = function()
+			-- === 1. 强力监控逻辑 ===
+			-- 创建一个自动命令组，防止重复
+			local group = vim.api.nvim_create_augroup("DashboardTabline", { clear = true })
+
+			-- 监听 "BufEnter" (进入缓冲区) 事件
+			vim.api.nvim_create_autocmd({ "BufEnter", "BufWinEnter" }, {
+				group = group,
+				callback = function()
+					-- 获取当前文件类型
+					local ft = vim.bo.filetype
+
+					-- 如果是 Dashboard，或者 Alpha (另一个启动页插件)，甚至是空的
+					if ft == "dashboard" or ft == "alpha" then
+						vim.opt.showtabline = 0 -- 隐藏
+					else
+						-- 只有当真的有 buffer 需要显示时才开启
+						-- 避免刚启动那一瞬间的闪烁
+						if #vim.fn.getbufinfo({ buflisted = 1 }) > 0 then
+							vim.opt.showtabline = 2 -- 显示
+						end
+					end
+				end,
+			})
+
+			-- === 2. Bufferline 常规配置 ===
 			require("bufferline").setup({
 				options = {
-					-- 风格设置
-					mode = "buffers", -- 显示 Buffer (文件)
-					style_preset = require("bufferline").style_preset.default,
-					separator_style = "slant", -- 像 VSCode 那样的斜角标签
+					mode = "buffers",
+					style_preset = require("bufferline").style_preset.no_italic,
+					separator_style = "slant",
 					always_show_bufferline = true,
-
-					-- 集成诊断信息 (比如文件有错，标签页上会显示红点)
 					diagnostics = "nvim_lsp",
-
-					-- 让左侧的文件树 (Neo-tree) 不被标签栏遮挡
+					diagnostics_indicator = function(count, level)
+						local icon = level:match("error") and " " or " "
+						return " " .. icon .. count
+					end,
 					offsets = {
 						{
 							filetype = "neo-tree",
-							text = "File Explorer",
+							text = "QFVIM Explorer",
 							highlight = "Directory",
 							text_align = "left",
+							separator = true,
 						},
 					},
+					close_command = function(n)
+						require("mini.bufremove").delete(n, false)
+					end,
+					right_mouse_command = function(n)
+						require("mini.bufremove").delete(n, false)
+					end,
 				},
 			})
 
-			-- === 快捷键设置 (最顺手的方案) ===
-
-			-- 1. 左右切换标签：使用 Shift + h / l
-			-- 这比去按 gt 或 gT 快多了，手不用离开主键盘区
-			vim.keymap.set("n", "<S-h>", ":BufferLineCyclePrev<CR>", { desc = "Prev Buffer" })
-			vim.keymap.set("n", "<S-l>", ":BufferLineCycleNext<CR>", { desc = "Next Buffer" })
-
-			-- 2. 关闭当前标签：使用 Space + x
-			-- (原生是 :bd，但映射一下更方便)
-			-- vim.keymap.set("n", "<Leader>x", ":bdelete<CR>", { desc = "Close Buffer" })
-			-- 修改后的代码：使用 mini.bufremove 智能关闭
+			-- === 3. 快捷键 ===
+			vim.keymap.set("n", "<S-h>", "<cmd>BufferLineCyclePrev<CR>", { desc = "Prev Buffer" })
+			vim.keymap.set("n", "<S-l>", "<cmd>BufferLineCycleNext<CR>", { desc = "Next Buffer" })
 			vim.keymap.set("n", "<Leader>x", function()
 				require("mini.bufremove").delete(0, false)
 			end, { desc = "Close Buffer" })
-
-			-- 3. 移动标签位置 (整理强迫症专用)
-			-- Shift + < 或 > 来左右移动标签
-			vim.keymap.set("n", "<S-Left>", ":BufferLineMovePrev<CR>", { desc = "Move Buffer Left" })
-			vim.keymap.set("n", "<S-Right>", ":BufferLineMoveNext<CR>", { desc = "Move Buffer Right" })
+			vim.keymap.set("n", "<S-Left>", "<cmd>BufferLineMovePrev<CR>", { desc = "Move Buffer Left" })
+			vim.keymap.set("n", "<S-Right>", "<cmd>BufferLineMoveNext<CR>", { desc = "Move Buffer Right" })
+			vim.keymap.set("n", "<Leader>bp", "<cmd>BufferLineTogglePin<CR>", { desc = "Toggle Pin" })
 		end,
 	},
 }
